39ebb5848b80f3ab3725eb9674081ec7b584c47e
Heather Miller <heather.miller114@gmail.com>
2019-01-10 19:32:30 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,11 +1,5 @@
 class PigLatinizer
 
-  attr_accessor :phrase
-
-  def initialize
-      @phrase = phrase.downcase
-  end
-
   def pig_latin_string
     words = phrase.split(" ")
     p = words.map {|word| pig_latin_word(word)}
