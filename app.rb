309384eb161dbba0a1d8f60e768b8e3af6667a92
Heather Miller <heather.miller114@gmail.com>
2019-01-10 19:37:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end
   
-  post '/piglatinizer' do
+  post '/piglatinize' do
     @pig_latined = PigLatinizer.new.piglatinize(params[:user_phrase])
     erb :results
   end
