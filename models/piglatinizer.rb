a5f6797ed4c609a49582f0e1d7aca76b37461182
Heather Miller <heather.miller114@gmail.com>
2019-01-10 18:55:22 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,26 +1,35 @@
 class PigLatinizer
-  
-  attr_accessor :phrase
-  
-  def initialize(phrase)
-    @phrase = phrase.downcase
-  end
-  
-  def translate(phrase)
-    words = phrase.split(" ")
-    alphabet = ('a'..'z').to_a
-    vowels = %w[a e i o u]
-    consonants = alphabet - vowels
-    
-    if vowels.include?(phrase[0])
-      input + 'ay'
-    elsif consonants.include?(phrase[0]) && consonants.include?(phrase[1])
-      phrase[2..-1] + phrase[0..1] + 'ay'
-    elsif consonants.include?(phrase[0])
-      phrase[1..-1] + phrase[0] + 'ay'
-    else
-      phrase
+
+    attr_accessor :phrase
+
+    def initialize(phrase)
+        @phrase = phrase.downcase
     end
-  end
-  
+
+    def translate(phrase)
+        vowels = ["a", "e", "i", "o", "u"]
+        two_letter_consonants = ["ch", "sh", "qu", "th", "br"]
+        three_letter_consonants = ["thr", "sch", "squ"]
+    
+        words = phrase.split(" ")
+        result = [];
+        
+        words.each do |word|
+            if vowels.include? word[0]
+                result.push word << 'ay'
+            else
+                if three_letter_consonants.include? word[0] + word[1] + word[2]
+                    first_three_letters = word.slice!(0,3)
+                    result.push word << first_three_letters << 'ay'                    
+                elsif  two_letter_consonants.include? word[0] + word[1]
+                    first_two_letters = word.slice!(0,2)
+                    result.push word << first_two_letters << 'ay'                    
+                else
+                    first_letter = word.slice!(0)
+                    result.push word << first_letter << 'ay'
+                end       
+            end  
+        end 
+        return result.join(" ")    
+    end
 end
